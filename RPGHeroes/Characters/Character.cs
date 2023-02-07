ba4b954932b431c81b899f2c60ad51e608df85c9
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2023-02-07 12:56:37 +0100

    Add project files.

---
+++ RPGHeroes/Characters/Character.cs
@@ -0,0 +1,14 @@
+﻿namespace RPGHeroes.Characters
+{
+    public abstract class Character
+    {
+        public int Level { get; private set; } = 1;
+        protected string Name { get; set; }
+        protected string CharacterClass { get; set; }
+
+        public void LevelUp()
+        {
+            Level++;
+        }
+    }
+}
---
+++ RPGHeroes/Characters/Rogue.cs
@@ -0,0 +1,19 @@
+﻿namespace RPGHeroes.Characters
+{
+    public class Rogue : Character
+    {
+        private int baseStr = 2;
+        private int baseDex = 6;
+        private int baseInt = 1;
+        private int lvlUpStr = 1;
+        private int lvlUpDex = 4;
+        private int lvlUpInt = 1;
+
+
+        public Rogue(string name)
+        {
+            Name = name;
+            CharacterClass = "Rogue";
+        }
+    }
+}
